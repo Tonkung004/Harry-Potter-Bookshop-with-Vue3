e22b380b00f88fe7efc49ec120093aef37c6a7bf
Tonkung004 <142674969+Tonkung004@users.noreply.github.com>
2025-02-14 15:49:59 +0700

    HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,8 @@
   <div class="container mx-auto p-4">
     <h1 class="text-2xl font-bold mb-4 text-center">Harry Potter Book Store</h1>
 
-    <!-- คอมโพเนนต์รายการหนังสือ -->
     <BookPage />
 
-    <!-- ปุ่มไปยังตะกร้าสินค้า -->
     <div class="mt-6 text-center">
       <router-link to="/cart" class="bg-blue-500 text-white px-4 py-2 rounded">
         Go to Cart
